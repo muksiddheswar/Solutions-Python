590e8d7e1b19029c78fac5e0e6a9b6231ef65ce9
konkeror <sid.mkjee@gmail.com>
2022-08-03 22:04:55 +0200

    Diagonal Difference

---
+++ InterviewPreparationKit/1WeekPreparationKits/Day2/DiagonalDifference.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    sum1 = 0
+    for i in range(len(arr)):
+        sum1 += arr[i][i] - arr[i][len(arr) - 1 - i]
+    return abs(sum1)
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = []
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+    result = diagonalDifference(arr)
+    print(result)
