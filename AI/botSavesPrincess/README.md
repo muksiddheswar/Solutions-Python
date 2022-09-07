322d87f440e228cfe3eee19717e1f03d52b677c0
konkeror <sid.mkjee@gmail.com>
2022-09-07 18:03:54 +0200

    Reordered

--- AI/botSavesPrincess/README.md
+++
@@ -1,1 +0,0 @@
-https://www.hackerrank.com/challenges/saveprincess/copy-from/141909558
--- python/string_split_and_join.py
+++
@@ -1,8 +0,0 @@
-def split_and_join(line):
-    s = line.split(" ")
-    return "-".join(s)
-
-if __name__ == '__main__':
-    line = input()
-    result = split_and_join(line)
-    print(result)
